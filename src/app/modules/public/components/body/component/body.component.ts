1d74c9b059ca7a67600e9444e476c4abe267c7c4
Willian Giacomelli <willian2406@live.com>
2025-02-12 21:38:48 -0300

    feat: add public body, header, navbar, and product components with associated styles and templates

---
+++ src/app/modules/public/components/body/component/body.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-public-body',
+  standalone: false,
+  templateUrl: './body.component.html',
+  styleUrl: './body.component.scss'
+})
+export class BodyComponent {
+
+}
--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './component/dashboard.component';
 import { PublicRoutes } from './public.routing';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/header/components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/component/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/component/body.component';
+import { ProductComponent } from './components/body/components/product/product.component';
 
 
 
@@ -13,7 +15,9 @@ import { FooterComponent } from './components/footer/footer.component';
     DashboardComponent,
     HeaderComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    BodyComponent,
+    ProductComponent
   ],
   imports: [
     CommonModule,
