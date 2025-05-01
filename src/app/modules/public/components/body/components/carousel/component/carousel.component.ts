14ea1f545ba6e2d930ee06429b419b6b84163de2
Willian Giacomelli <willian2406@live.com>
2025-05-01 17:29:14 -0300

    feat: Add carousel and see-also components with responsive product display

--- src/app/modules/public/components/body/components/carousel/component/carousel.component.ts
+++ src/app/modules/public/components/body/components/carousel/component/carousel.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
@@ -7,10 +15,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
+  public products: Product[] = [
+    {
+      id:1,
+      title: "Monitor gamer",
+      price: 2500,
+      description:"Monitor gamer Acer 180FPS",
+      image: "https://img.freepik.com/fotos-gratis/vista-do-monitor-de-exibicao-de-video-do-computador_23-2150757345.jpg?semt=ais_hybrid"
+    },
+    {
+      id:2,
+      title: "Iphone 16",
+      price: 4600,
+      description:"Iphone 16 128GB",
+      image: "https://img.freepik.com/fotos-gratis/vista-do-monitor-de-exibicao-de-video-do-computador_23-2150757345.jpg?semt=ais_hybrid"
+    },
+    {
+      id:3,
+      title: "Iphone 16",
+      price: 4600,
+      description:"Iphone 16 128GB",
+      image: "https://img.freepik.com/fotos-gratis/vista-do-monitor-de-exibicao-de-video-do-computador_23-2150757345.jpg?semt=ais_hybrid"
+    },
+    {
+      id:4,
+      title: "Iphone 16",
+      price: 4600,
+      description:"Iphone 16 128GB",
+      image: "https://img.freepik.com/fotos-gratis/vista-do-monitor-de-exibicao-de-video-do-computador_23-2150757345.jpg?semt=ais_hybrid"
+    },
+  ];
 
-  constructor() { }
+  public currentIndex = 0;
+  public itemsPerSlide = 3;
+  public totalSlides: number;
 
-  ngOnInit() {
+  constructor() {
+    this.totalSlides = Math.ceil(this.products.length / this.itemsPerSlide);
+    console.log(this.totalSlides)
+  }
+
+  ngOnInit(): void {
+    this.updateItemsPerSlide();
+    window.addEventListener('resize', () => this.updateItemsPerSlide());
+  }
+
+  updateItemsPerSlide(): void {
+    const width = window.innerWidth;
+    if (width < 640) {
+      this.itemsPerSlide = 1;
+    } else if (width < 1024) {
+      this.itemsPerSlide = 2;
+    } else {
+      this.itemsPerSlide = 3;
+    }
+    this.totalSlides = Math.ceil(this.products.length / this.itemsPerSlide);
+    this.currentIndex = Math.min(this.currentIndex, this.totalSlides - 1);
+  }
+
+  prevSlide(): void {
+    this.currentIndex = (this.currentIndex > 0) ? this.currentIndex - 1 : this.totalSlides - 1;
+  }
+
+  nextSlide(): void {
+    this.currentIndex = (this.currentIndex < this.totalSlides - 1) ? this.currentIndex + 1 : 0;
+  }
+
+  goToSlide(index: number): void {
+    this.currentIndex = index;
+  }
+
+  getVisibleProducts(): Product[] {
+    const visibleProducts: Product[] = [];
+    const startIndex = this.currentIndex * this.itemsPerSlide;
+
+    for (let i = 0; i < this.itemsPerSlide; i++) {
+      const productIndex = (startIndex + i) % this.products.length;
+      visibleProducts.push(this.products[productIndex]);
+    }
+
+    return visibleProducts;
   }
 
 }
